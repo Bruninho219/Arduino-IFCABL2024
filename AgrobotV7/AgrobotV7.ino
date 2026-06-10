8329cf2f5f973a5bf5455d68ad97ba2dc7d1d6e8
Bruno Rosa da Silva <brunodarosasilva.brs@hotmail.com>
2026-06-10 09:46:58 -0300

    Update AgrobotV7.ino

--- AgrobotV7/AgrobotV7.ino
+++ AgrobotV7/AgrobotV7.ino
@@ -1,20 +1,20 @@
 #include <Ultrasonic.h>
 
-#define pinoUST 4
-#define pinoUSE 5
+#define pinoUST 5
+#define pinoUSE 4
 #define pinoSensorIVD 3
 #define pinoSensorIVE 2
 
-const uint8_t m1=0; 
-const uint8_t m2=8;
+const uint8_t m1=0; //esquerdo
+const uint8_t m2=20; //direito
 int i=0;
 
 //Ultrasonic ultrasonic(pinoUST, pinoUSE);
-Ultrasonic ultrasonic(4, 5);
+Ultrasonic ultrasonic(pinoUST, pinoUSE);
 
 class DCMotor
 {
-  uint8_t spd = 150, pin1, pin2;
+  uint8_t spd = 100, pin1, pin2;
   
   public:
   void Pinout(uint8_t in1, uint8_t in2)
@@ -27,14 +27,21 @@ class DCMotor
 
   void Frente(uint8_t x)
   {
-    analogWrite(pin1, spd+x);
+    if((spd+x)>255)
+      analogWrite(pin1, 255);
+    else
+      analogWrite(pin1, spd+x);
     digitalWrite(pin2, LOW);
   }
 
   void Tras(uint8_t x)
   {
+    if((spd+x)>255)
+      analogWrite(pin2, 255);
+    else
+      analogWrite(pin2, spd+x);
     digitalWrite(pin1, LOW);
-    analogWrite(pin2, spd+x);
+
   }
 
   void Parada()
@@ -59,9 +66,11 @@ void Direcao(int E, int D)
   Branco=0
   Preto=1
   */
-  uint8_t dlRe=50;
-  uint8_t dlGiro=75;
-  uint8_t dlRandom=50;
+  uint8_t dlRe=500;
+  uint8_t dlGiro=500;
+  uint8_t dlRandom=150;
+  uint8_t PotenciaGiro=100;
+  
   
   if (E == 1 && D == 1)
   {
@@ -82,8 +91,8 @@ void Direcao(int E, int D)
     Motor2.Tras(m2);
     delay(dlRe);
     //Direito
-    Motor1.Tras(m1);
-    Motor2.Frente(m2);
+    Motor1.Tras(m1+PotenciaGiro);
+    Motor2.Frente(m2+PotenciaGiro);
     delay(dlGiro+random(dlRandom));
 
   }
@@ -93,8 +102,8 @@ void Direcao(int E, int D)
     Motor2.Tras(m2);
     delay(dlRe);
     //Esquerdo
-    Motor1.Frente(m1);
-    Motor2.Tras(m2);
+    Motor1.Frente(m1+PotenciaGiro);
+    Motor2.Tras(m2+PotenciaGiro);
     delay(dlGiro+random(dlRandom));
   }
   else
@@ -103,125 +112,158 @@ void Direcao(int E, int D)
 
 void Desvio()
 {
-  int distanciaMinima=20;
-  //long microsec = ultrasonic.timing();
-  digitalWrite(pinoUST, LOW);
-  delayMicroseconds(2);
+  int distanciaMinima = 20;
 
-  digitalWrite(pinoUST, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(pinoUST, LOW);
+  long distancia = ultrasonic.Ranging(CM);
 
-  long duration = pulseIn(pinoUSE, HIGH, 30000);
-
-  /*float distancia = ultrasonic.convert(microsec, Ultrasonic::CM);
-  int distancia = ultrasonic.read();
-  */
-    
-  long distancia = duration * 0.034 / 2;
   Serial.print("Distancia: ");
   Serial.print(distancia);
   Serial.println(" cm");
 
-  if(distancia<distanciaMinima)
+  if(distancia == 0)
   {
-      Serial.print("Obstáculo detectado a menos de ");
-      Serial.print(distanciaMinima);
-      Serial.println("cm!");
-      obstaculo();
+    distancia = 21;
   }
-  else Serial.println("Obstáculo não detectado!");
+
+  if(distancia < distanciaMinima)
+  {
+    Serial.print("Obstáculo detectado a menos de ");
+    Serial.print(distanciaMinima);
+    Serial.println(" cm!");
+    obstaculo();
+  }
+  else
+  {
+    Serial.println("Obstáculo não detectado!");
+  }
 }
 
 void obstaculo()
 {
+    digitalWrite(20, HIGH); // acende led amarelo
+    digitalWrite(21, HIGH); // acende led vermelho
+    uint8_t PotenciaGiro=50;
+
     digitalWrite(13, HIGH);
     int giro90=550;
-    int frente=750;
+    int frente=1250;
     int avanca=2500;
 
     Serial.println("{Iniciado o desvio!}");
 
-   /* Serial.println("01 Para");
+    delay(50);
+    Serial.println("01 Para");
     Motor1.Parada();
     Motor2.Parada();
+    
     delay(100);
 
+    digitalWrite(20, LOW); // apaga led amarelo
     Serial.println("02 Vira");
-    Motor1.Tras(m1);
-    Motor2.Frente(m2);
+    Motor1.Tras(m1+PotenciaGiro);
+    Motor2.Frente(m2+PotenciaGiro);
     delay(giro90);
 
+    digitalWrite(20, HIGH); // acende led amarelo
+    digitalWrite(21, LOW);  // apaga led vermelho
     Serial.println("03 Para");
     Motor1.Parada();
     Motor2.Parada();
     delay(100);
-*/
+
+    digitalWrite(20, LOW);   // apaga led amarelo
+    digitalWrite(21, HIGH);  // acende led vermelho
     Serial.println("04 Frente");
     Motor1.Frente(m1);
     Motor2.Frente(m2);
     delay(frente);
-/*
+
+    digitalWrite(20, HIGH); // acende led amarelo
+    digitalWrite(21, LOW);  // apaga led vermelho
     Serial.println("05 Para");
     Motor1.Parada();
     Motor2.Parada();
     delay(100);
 
+    digitalWrite(20, LOW);   // apaga led amarelo
+    digitalWrite(21, HIGH);  // acende led vermelho
     Serial.println("06 Vira");
-    Motor1.Frente(m1);
-    Motor2.Tras(m2);
+    Motor1.Frente(m1+PotenciaGiro);
+    Motor2.Tras(m2+PotenciaGiro);
     delay(giro90);
 
+    digitalWrite(20, HIGH); // acende led amarelo
+    digitalWrite(21, LOW);  // apaga led vermelho
     Serial.println("07 Para");
     Motor1.Parada();
     Motor2.Parada();
     delay(100);
 
+    digitalWrite(20, LOW);   // apaga led amarelo
+    digitalWrite(21, HIGH);  // acende led vermelho
     Serial.println("08 Frente");
     Motor1.Frente(m1);
     Motor2.Frente(m2);
     delay(avanca);
 
+    digitalWrite(20, HIGH); // acende led amarelo
+    digitalWrite(21, LOW);  // apaga led vermelho
     Serial.println("09 Para");
     Motor1.Parada();
     Motor2.Parada();
     delay(100);
 
+    digitalWrite(20, LOW);   // apaga led amarelo
+    digitalWrite(21, HIGH);  // acende led vermelho
     Serial.println("10 Vira");
-    Motor1.Frente(m1);
-    Motor2.Tras(m2);
+    Motor1.Frente(m1+PotenciaGiro);
+    Motor2.Tras(m2+PotenciaGiro);
     delay(giro90);
 
+    digitalWrite(20, HIGH); // acende led amarelo
+    digitalWrite(21, LOW);  // apaga led vermelho
     Serial.println("11 Para");
     Motor1.Parada();
     Motor2.Parada();
     delay(100);
 
+    digitalWrite(20, LOW);   // apaga led amarelo
+    digitalWrite(21, HIGH);  // acende led vermelho
     Serial.println("12 Frente");
     Motor1.Frente(m1);
     Motor2.Frente(m2);
     delay(frente);
 
+    digitalWrite(20, HIGH); // acende led amarelo
+    digitalWrite(21, LOW);  // apaga led vermelho
     Serial.println("13 Para");
     Motor1.Parada();
     Motor2.Parada();
     delay(100);
 
+    digitalWrite(20, LOW);   // apaga led amarelo
+    digitalWrite(21, HIGH);  // acende led vermelho
     Serial.println("14 Vira");
-    Motor1.Tras(m1);
-    Motor2.Frente(m2);
+    Motor1.Tras(m1+PotenciaGiro);
+    Motor2.Frente(m2+PotenciaGiro);
     delay(giro90);
 
+    digitalWrite(20, HIGH); // acende led amarelo
     Serial.println("15 Para");
     Motor1.Parada();
     Motor2.Parada();
-    delay(100);
+    delay(150);
     digitalWrite(13, LOW);
-    */
+
+    digitalWrite(20, LOW);  // apaga led amarelo
+    digitalWrite(21, LOW);  // apaga led vermelho    
 }
 
 void setup()
 {
+  pinMode(20, OUTPUT);   // led amarelo
+  pinMode(21, OUTPUT);   // led vermelho
+
   pinMode(pinoUST, OUTPUT);
   pinMode(pinoUSE, INPUT);
 
@@ -229,8 +271,8 @@ void setup()
   pinMode(pinoSensorIVE, INPUT);
   pinMode(pinoSensorIVD, INPUT);
 
-  Motor1.Pinout(8, 9);
-  Motor2.Pinout(6, 7);
+  Motor1.Pinout(8, 9); //esquerdo
+  Motor2.Pinout(7, 6); //direito
 
   Serial.begin(9600);
 }
@@ -239,7 +281,7 @@ void loop()
   int valorIVD = digitalRead(pinoSensorIVD);
   int valorIVE = digitalRead(pinoSensorIVE);
 
-  //Desvio();
+  Desvio();
   Direcao(valorIVE, valorIVD);
   delay(50);
   Serial.print("Cont: ");
